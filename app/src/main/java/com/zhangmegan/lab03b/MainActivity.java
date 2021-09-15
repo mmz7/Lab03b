c482a82fdf9fb5e01b1fa3cedca67d181e61004d
mmz7 <mmzhang711@gmail.com>
2021-09-15 13:33:45 -0400

    seekbar added

--- app/src/main/java/com/zhangmegan/lab03b/MainActivity.java
+++ app/src/main/java/com/zhangmegan/lab03b/MainActivity.java
@@ -1,25 +1,32 @@
 package com.zhangmegan.lab03b;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView nameLabel;
     EditText nameInput;
     Button button;
+    ConstraintLayout layout;
     SharedPreferences sP;
     SharedPreferences.Editor editor;
+    SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        layout = findViewById(R.id.layout);
+        seekBar = findViewById(R.id.seekbar);
         nameLabel = findViewById(R.id.nameLabel);
         nameInput = findViewById(R.id.nameInput);
         button = findViewById(R.id.button);
@@ -29,8 +36,29 @@ public class MainActivity extends AppCompatActivity {
                 String name = nameInput.getText().toString();
                 sP = getSharedPreferences(name, MODE_PRIVATE);
                 editor = sP.edit();
+                int gn = sP.getInt("G", 0);
+                seekBar.setProgress(gn);
+            }
+        });
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int color = Color.argb(255, 255, i, 0);
+                layout.setBackgroundColor(color);
+                editor.putInt("G", seekBar.getProgress());
+                editor.apply();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
         });
     }
+
 }
